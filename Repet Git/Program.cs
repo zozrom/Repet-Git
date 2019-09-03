827b2a06a23c5dd93342bbacaa8702483f02181e
zozrom <zozulia.roman135@gmail.com>
2019-09-03 19:19:52 +0300

    Добавьте файлы проекта.

---
+++ Repet Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repet_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
